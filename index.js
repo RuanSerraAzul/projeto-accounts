003d375591dec536720578d512bac721fc72cff9
Ruan Serra Azul <ruan.4321.pablo@gmail.com>
2022-10-12 19:24:56 -0300

    programa exibindo opções (ainda não funcionais)

--- index.js
+++ index.js
@@ -6,3 +6,25 @@ const chalk = require("chalk");
 const fs = require("fs");
 
 console.log("...Iniciando Accounts...");
+
+operation();
+
+function operation() {
+    inquirer
+        .prompt([
+            {
+                type: "list",
+                name: "action",
+                message: "O que você deseja fazer?",
+                choices: [
+                    "Criar conta",
+                    "Consultar saldo",
+                    "Depositar",
+                    "Sacar",
+                    "Sair",
+                ],
+            },
+        ])
+        .then()
+        .catch((err) => console.log(err));
+}
