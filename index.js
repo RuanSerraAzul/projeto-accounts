b8b141f33da2a4a0eaf2526b46974ea817f032b9
Ruan Serra Azul <ruan.4321.pablo@gmail.com>
2022-10-13 11:10:42 -0300

    função criar conta concluida

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ function operation() {
 
             if (action === "Criar conta") {
                 criarConta();
+                construirConta();
             }
         })
         .catch((err) => console.log(err));
@@ -40,4 +41,45 @@ function operation() {
 function criarConta() {
     console.log(chalk.bgGreen.black("Obrigado por utilizar o Accounts!"));
     console.log(chalk.green("Defina as opções da sua conta a seguir:"));
+}
+
+function construirConta() {
+    inquirer
+        .prompt([
+            {
+                name: "accountName",
+                message: "Digite um nome para a sua conta:",
+            },
+        ])
+        .then((answer) => {
+            const accountName = answer["accountName"];
+
+            console.info(accountName);
+
+            if (!fs.existsSync("accounts")) {
+                fs.mkdirSync("accounts");
+            }
+
+            if (fs.existsSync(`accounts/${accountName}.json`)) {
+                console.log(
+                    chalk.bgRed.black(
+                        "Erro, conta já existe, escolha outro nome"
+                    )
+                );
+                construirConta();
+            }
+
+            fs.writeFileSync(
+                `accounts/${accountName}.json`,
+                '{"balance":0}',
+                function (err) {
+                    console.log(err);
+                }
+            );
+
+            console.log(chalk.green("Conta criada com sucesso!"));
+            operation();
+        })
+
+        .catch((err) => console.log(err));
 }
